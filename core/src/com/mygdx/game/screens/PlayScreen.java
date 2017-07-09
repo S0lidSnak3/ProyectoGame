1199e7f96b2a8b290c5f9169c418194e1d257802
newluis_90 <newluis_90@macbook-de-luis.lan>
2017-07-08 23:07:17 -0600

    Luis Salazar estuvo aqui

--- core/src/com/mygdx/game/screens/PlayScreen.java
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -5,11 +5,14 @@
  */
 package com.mygdx.game.screens;
 
+import Scenes.Hud;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.*;
 import com.mygdx.game.MyGdxGame;
     
 /**
@@ -17,11 +20,15 @@ import com.mygdx.game.MyGdxGame;
  * @author lsalazar
  */
 public class PlayScreen implements Screen {
+    
     private MyGdxGame game;
-    Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
     public PlayScreen(MyGdxGame game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");        
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH,MyGdxGame.V_HEIGHT,gamecam);
     }
     @Override
     public void show() {
@@ -32,14 +39,13 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }   
 
     @Override
     public void resize(int width, int height) {
-       
+       gamePort.update(width, height);
     }
 
     @Override
