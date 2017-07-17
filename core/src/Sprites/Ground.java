ef8820618c924cf5c36c71a9c479bf93d7e5eb13
newluis_90 <newluis_90@macbook-de-luis.lan>
2017-07-16 22:36:55 -0600

    Actualizacion de mario bros

---
+++ core/src/Sprites/Ground.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sprites;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ *
+ * @author newluis_90
+ */
+public class Ground extends InteractiveTileObject {
+    
+    public Ground(World world, TiledMap map, Rectangle bounds) {
+        super(world, map, bounds);
+    }
+    
+}
---
+++ core/src/Tools/B2WorldCreator.java
@@ -0,0 +1,54 @@
+
+package Tools;
+
+import Sprites.Bricks;
+import Sprites.Coin;
+import Sprites.Ground;
+import Sprites.Tubes;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.MyGdxGame;
+
+public class B2WorldCreator {
+
+    public B2WorldCreator(World world, TiledMap map){
+        
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+           //Creando el suelo y sus fixtures
+        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            
+            new Ground(world,map,rect);
+            
+        }
+        //Creando los tubos y sus fixtures
+        for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            
+          new Tubes(world, map, rect);
+        }
+        //Creando los ladrillos y sus fixtures
+        for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            
+            new Bricks(world,map,rect);
+        }
+        //Creando las monedas y sus fixtures
+        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            
+            new Coin(world,map,rect);
+        }
+    }
+    
+}
